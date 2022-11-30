67f459edd7831e56bf91ea855457d563e5c837b7
Benhur <benhurcontel@gmail.com>
2022-11-30 16:23:56 -0300

    aaa

--- includes/corpo.php
+++ includes/corpo.php
@@ -80,7 +80,7 @@
                 </a>
             </div>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Cartãp</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="receita.php" class="btn btn-primary container">Button</a>
             </div>
